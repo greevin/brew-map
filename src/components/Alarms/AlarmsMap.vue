a668117c3d040ecf940cd4456c025822d60fff6e
Cícero Neto <cicero.alex@gmail.com>
2020-12-25 15:14:38 -0300

    Added clusters

--- src/components/Alarms/AlarmsMap.vue
+++ src/components/Alarms/AlarmsMap.vue
@@ -23,8 +23,10 @@
           >
         </div>
       </GmapInfoWindow>
-      <!-- marker -->
-      <!-- <GmapMarker
+
+      <GmapCluster :zoom-on-click="true">
+        <!-- marker -->
+        <!-- <GmapMarker
         :clickable="true"
         :draggable="false"
         v-for="alarm in allAlarmList"
@@ -34,31 +36,31 @@
         :label="alarm.icon"
         @click="handleMarkerClicked(alarm)"
       /> -->
-      <gmap-custom-marker
-        :clickable="true"
-        v-for="alarm in allAlarmList"
-        :key="alarm.id"
-        :marker="getPosition(alarm)"
-        :icon="markerOptions"
-        :label="alarm.icon"
-        @click.native="handleMarkerClicked(alarm)"
-      >
-        <b-iconstack font-scale="4">
-          <b-icon
-            stacked
-            icon="circle-fill"
-            :style="'color:' + alarm.color_line"
-          ></b-icon>
-          <b-icon
-            stacked
-            :icon="alarm.icon"
-            scale="0.5"
-            variant="grey"
-          ></b-icon>
-          <b-icon stacked icon="circle" variant="danger"></b-icon>
-        </b-iconstack>
+        <gmap-custom-marker
+          :clickable="true"
+          v-for="alarm in allAlarmList"
+          :key="alarm.id"
+          :marker="getPosition(alarm)"
+          :icon="markerOptions"
+          :label="alarm.icon"
+          @mouseenter.native.stop="handleMarkerClicked(alarm)"
+        >
+          <b-iconstack font-scale="4">
+            <b-icon
+              stacked
+              icon="circle-fill"
+              :style="'color:' + alarm.color_line"
+            ></b-icon>
+            <b-icon
+              stacked
+              :icon="alarm.icon"
+              scale="0.5"
+              variant="grey"
+            ></b-icon>
+            <b-icon stacked icon="circle" variant="danger"></b-icon>
+          </b-iconstack>
 
-        <!-- <b-icon
+          <!-- <b-icon
           v-if="alarm.icon"
           :icon="alarm.icon"
           :style="'color:' + alarm.color_line"
@@ -69,7 +71,8 @@
           icon="exclamation-circle"
           style="width: 35px; height: 35px"
         ></b-icon> -->
-      </gmap-custom-marker>
+        </gmap-custom-marker>
+      </GmapCluster>
     </GmapMap>
   </div>
 </template>
@@ -78,12 +81,14 @@
 // import securityCamera from "../assets/security-camera.png";
 import { mapGetters, mapState } from "vuex";
 import GmapCustomMarker from "vue2-gmap-custom-marker";
+import GmapCluster from "vue2-google-maps/dist/components/cluster";
 const icon =
   '<svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="exclamation triangle fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-exclamation-triangle-fill b-icon bi"><g><path fill-rule="evenodd" d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 5zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path></g></svg>';
 
 export default {
   components: {
     GmapCustomMarker,
+    GmapCluster,
   },
   computed: {
     ...mapState(["lat", "lng", "zoom"]),
@@ -135,4 +140,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+div[clickable="true"] {
+  cursor: pointer;
+}
 </style>
