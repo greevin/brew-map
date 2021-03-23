4f851786fae48b9cc7038529570a0494a78c563a
Cícero Neto <cicero.alex@gmail.com>
2021-03-23 12:06:54 -0300

    Fix if else

--- src/store/modules/alarmsv2.js
+++ src/store/modules/alarmsv2.js
@@ -15,9 +15,18 @@ export default {
       return allAlarms.filter(latitude => latitude.latitude != null);
     },
     allAlarm: state => {
-      const alarm = state.alarmedoffices.filter(latitude => latitude.latitude != null);
+      const alarm = state.alarmedoffices.filter(
+        latitude => latitude.latitude != null
+      );
 
-      return alarm.sort((a, b) => (a.priority_id > b.priority_id ? 1 : -1)).reverse();
+      return alarm.sort((a, b) => {
+        if (a.priority_id > b.priority_id) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }).reverse();
+
     }
   },
   mutations: {
@@ -28,7 +37,7 @@ export default {
     }
   },
   actions: {
-    async getAllAlarms({commit}) {
+    async getAllAlarms({ commit }) {
       await axios.get(path).then(res => {
         commit('setAlarmsList', res.data);
       });
